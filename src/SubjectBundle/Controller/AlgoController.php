7994552647ba868a63f16494ca715b514045c4a9
youngdreamer33 <b92.a75@gmail.com>
2017-04-19 14:18:14 +0200

    Envoi algo (pas fait de commit avant le push en partant.....)

--- src/SubjectBundle/Controller/AlgoController.php
+++ src/SubjectBundle/Controller/AlgoController.php
@@ -6,12 +6,34 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class AlgoController extends Controller
 {
-    //////////////////////////////////////
-    // Complète la fonction suivante //
-    //////////////////////////////////////
-    //
-    public function strlenOrder($string, $order)
-    {
+    public function strlenOrder($string, $order) {
 
+        $word = explode(" ", $string);
+
+        if ($order === 'ASC'){
+            function orderAsc($word,$b){
+                return (strlen($word) > strlen($b)) ? 1 : -1;
+            }
+
+            usort($word, "orderAsc");
+
+            foreach ($word as $key => $value) {
+                echo "$value ";
+            }
+        }
+
+        elseif ($order ==='DESC') {
+            function orderDesc($word,$b){
+                return (strlen($word) < strlen($b)) ? 1 : -1;
+            }
+            usort($word, "orderDesc");
+
+            foreach ($word as $key => $value) {
+                echo "$value ";
+            }
+        }
+
+        else  {
+            echo 'Il y a eu un problème !'; }
     }
 }
